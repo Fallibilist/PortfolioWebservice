f616042c7570fcac32e6d88c6a6b7d2427cf348b
Fallbilist <Greg.marshall.hill@gmail.com>
2017-10-18 12:57:27 -0700

    Expanded file structure and designed state structure

---
+++ ui/src/main/webapp/js/portfolio/portfolio.module.js
@@ -0,0 +1,109 @@
+/**
+ * Imports modules containing state information
+ */
+import apiUrl from './api.url'
+
+import session from './session/session.module'
+
+import sessionAbout from './session/about/about.module'
+import sessionAboutBio from './session/about/bio/bio.module'
+import sessionAboutEducation from './session/about/education/education.module'
+import sessionAboutInterests from './session/about/interests/interests.module'
+import sessionAboutResume from './session/about/resume/resume.module'
+
+import sessionProjects from './session/projects/projects.module'
+import sessionProjectsContent from './session/projects/content/content.module'
+import sessionProjectsInfo from './session/projects/info/info.module'
+
+import sessionLinks from './session/links/links.module'
+
+/**
+ * Exports the primary module which defines all states
+ */
+export default
+angular
+  .module('portfolio', [
+    'ui.router'
+  ])
+  .config(['$stateProvider', '$urlRouterProvider',
+    function ($stateProvider, $urlRouterProvider) {
+
+      const sessionState = {
+        abstract: true,
+        name: 'session',
+        url: '/session',
+        component: 'sessionComponent'
+      }
+
+      const aboutState = {
+        abstract: true,
+        name: 'session.about',
+        url: '/about',
+        component: 'aboutComponent'
+      }
+
+      const bioState = {
+        name: 'session.about.bio',
+        url: '/bio',
+        component: 'bioComponent'
+      }
+
+      const educationState = {
+        name: 'session.about.education',
+        url: '/education',
+        component: 'educationComponent'
+      }
+
+      const interestsState = {
+        name: 'session.about.interests',
+        url: '/interests',
+        component: 'interestsComponent'
+      }
+
+      const resumeState = {
+        name: 'session.about.resume',
+        url: '/resume',
+        component: 'resumeComponent'
+      }
+
+      const projectsState = {
+        abstract: true,
+        name: 'session.projects',
+        url: '/projects',
+        component: 'projectsComponent'
+      }
+      
+      const contentState = {
+        name: 'session.projects.content',
+        url: '/content',
+        component: 'contentComponent'
+      }
+      
+      const infoState = {
+        name: 'session.projects.info',
+        url: '/info',
+        component: 'infoComponent'
+      }
+
+      const linksState = {
+        name: 'session.links',
+        url: '/links',
+        component: 'linksComponent'
+      }
+
+      $stateProvider.state(sessionState)
+                      .state(aboutState)
+                        .state(bioState)
+                        .state(educationState)
+                        .state(interestsState)
+                        .state(resumeState)
+                      .state(projectsState)
+                        .state(contentState)
+                        .state(infoState)
+                      .state(linksState)
+
+      $urlRouterProvider.otherwise('/session/about/bio')
+    }
+  ])
+  .constant('apiUrl', apiUrl)
+  .name
